3c9c3535eb39ed278c41986930f30c8329d7ede5
solitariewolf <watanasio@gmail.com>
2023-06-14 22:00:41 -0300

    Modificado o layout e adicionado mais botões, e também começado o horizonte artificial
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   simulator/index.html
    	modified:   simulator/script.js
    	modified:   simulator/styles.css

--- simulator/script.js
+++ simulator/script.js
@@ -1,31 +1,95 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // elementos iniciais
     const spaceship = document.querySelector('.spaceship-frame img');
     let rotationDegrees = 0;
     let yawDegrees = 0;
     let pitchDegrees = 0;
 
-    //  listeners de eventos para o joystick e controles de guinada/inclinação
-    document.getElementById('joystick').addEventListener('input', function(event) {
+    const rotationDisplay = document.getElementById('rotation-display');
+    const yawDisplay = document.getElementById('yaw-display');
+    const pitchDisplay = document.getElementById('pitch-display');
+
+    const joystick = document.getElementById('joystick');
+    const yawControl = document.getElementById('yaw-control');
+    const pitchControl = document.getElementById('pitch-control');
+
+    joystick.addEventListener('input', updateRotation);
+    yawControl.addEventListener('input', updateYaw);
+    pitchControl.addEventListener('input', updatePitch);
+
+    const lockButtons = document.querySelectorAll('.lock-button');
+    lockButtons.forEach(button => {
+        button.addEventListener('click', toggleLock);
+    });
+
+    function toggleLock(event) {
+        const control = document.getElementById(event.target.dataset.control);
+        const light = document.getElementById(event.target.dataset.control + '-light');
+    
+        if (control.disabled) {
+            control.disabled = false;
+            light.classList.remove('red');
+            light.classList.add('green');
+            event.target.textContent = "L";
+        } else {
+            control.disabled = true;
+            light.classList.remove('green');
+            light.classList.add('red');
+            event.target.textContent = "L";
+        }
+    }
+
+    function updateRotation(event) {
         rotationDegrees = event.target.value - 180;
+        rotationDisplay.textContent = event.target.value;
         updateTransform();
-    });
+    }
 
-    document.getElementById('yaw-control').addEventListener('input', function(event) {
+    function updateYaw(event) {
         yawDegrees = event.target.value - 180;
+        yawDisplay.textContent = event.target.value;
         updateTransform();
-    });
+        const artificialHorizon = document.getElementById('artificial-horizon');
+        artificialHorizon.style.transform = `rotateX(${pitchDegrees}deg) rotateZ(${-yawDegrees}deg)`;
+    }
 
-    document.getElementById('pitch-control').addEventListener('input', function(event) {
+    function updatePitch(event) {
         pitchDegrees = event.target.value - 180;
+        pitchDisplay.textContent = event.target.value;
         updateTransform();
-    });
+        const artificialHorizon = document.getElementById('artificial-horizon');
+        artificialHorizon.style.transform = `rotateX(${pitchDegrees}deg) rotateZ(${-yawDegrees}deg)`;
+    }
 
-    // delay
     function updateTransform() {
         spaceship.style.transform = `translate(-50%, -50%) rotate(${rotationDegrees}deg) rotateY(${yawDegrees}deg) rotateX(${pitchDegrees}deg)`;
     }
 });
+
+document.querySelectorAll('.increment-button').forEach(button => {
+    button.addEventListener('click', function(event) {
+        const control = document.getElementById(event.target.dataset.control);
+        if (control.value < 360 && !control.disabled) {
+            control.value = parseInt(control.value, 10) + 1;
+            triggerInputEvent(control);
+        }
+    });
+});
+
+document.querySelectorAll('.decrement-button').forEach(button => {
+    button.addEventListener('click', function(event) {
+        const control = document.getElementById(event.target.dataset.control);
+        if (control.value > 0 && !control.disabled) {
+            control.value = parseInt(control.value, 10) - 1;
+            triggerInputEvent(control);
+        }
+    });
+});
+
+function triggerInputEvent(element) {
+    const event = new Event('input');
+    element.dispatchEvent(event);
+}
+
 // display multifuncional
 document.addEventListener('DOMContentLoaded', function() {
     const spaceship = document.querySelector('.spaceship-frame img');
