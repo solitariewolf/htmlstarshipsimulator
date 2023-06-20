b3e9a05839c2d50b4bd82151b608e0d6db161e4d
solitariewolf <watanasio@gmail.com>
2023-06-19 21:27:10 -0300

    ínicio do backend para fazer a nave funcionar
     Changes to be committed:
    	modified:   simulator/computer.js
    	modified:   simulator/styles.css

--- simulator/computer.js
+++ simulator/computer.js
@@ -1,70 +1,70 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Criação da estrutura do mini-computador
-  const miniComputer = document.createElement('div');
-  miniComputer.classList.add('mini-computer');
-  
-  const miniComputerScreen = document.createElement('div');
-  miniComputerScreen.classList.add('mini-computer-screen');
-  
-  const computerOutput = document.createElement('pre');
-  computerOutput.id = 'computer-output';
-  
-  const keyboard = document.createElement('div');
-  keyboard.classList.add('keyboard');
-  
-  const numericKeyboard = document.createElement('div');
-  numericKeyboard.classList.add('numeric-keyboard');
-  
-  miniComputerScreen.appendChild(computerOutput);
-  miniComputer.appendChild(miniComputerScreen);
-  miniComputer.appendChild(keyboard);
-  miniComputer.appendChild(numericKeyboard);
+    // Criação da estrutura do mini-computador
+    const miniComputer = document.createElement('div');
+    miniComputer.classList.add('mini-computer');
+    
+    const miniComputerScreen = document.createElement('div');
+    miniComputerScreen.classList.add('mini-computer-screen');
+    
+    const computerOutput = document.createElement('pre');
+    computerOutput.id = 'computer-output';
+    
+    const keyboard = document.createElement('div');
+    keyboard.classList.add('keyboard');
+    
+    const numericKeyboard = document.createElement('div');
+    numericKeyboard.classList.add('numeric-keyboard');
+    
+    miniComputerScreen.appendChild(computerOutput);
+    miniComputer.appendChild(miniComputerScreen);
+    miniComputer.appendChild(keyboard);
+    miniComputer.appendChild(numericKeyboard);
 
-  // Criação das teclas
-  const keys = [
-      ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-      ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-      ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
-      ['Espaço', 'Enter', 'Backspace']
-  ];
+    // Criação das teclas
+    const keys = [
+        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+        ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+        ['Espaço', 'Enter', 'Backspace']
+    ];
 
-  const numericKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const numericKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-  keys.forEach(row => {
-      const keyboardRow = document.createElement('div');
-      keyboardRow.classList.add('keyboard-row');
-      row.forEach(key => {
-          const button = document.createElement('button');
-          button.textContent = key;
-          if (key === 'Espaço') {
-              button.classList.add('space');
-          } else if (key === 'Enter') {
-              button.classList.add('enter');
-          } else if (key === '<Backspace>') {
-              button.classList.add('<backspace>');
-          }
-          keyboardRow.appendChild(button);
-      });
-      keyboard.appendChild(keyboardRow);
-  });
+    keys.forEach(row => {
+        const keyboardRow = document.createElement('div');
+        keyboardRow.classList.add('keyboard-row');
+        row.forEach(key => {
+            const button = document.createElement('button');
+            button.textContent = key;
+            if (key === 'Espaço') {
+                button.classList.add('space');
+            } else if (key === 'Enter') {
+                button.classList.add('enter');
+            } else if (key === 'Backspace') { // Modificado aqui
+                button.classList.add('backspace'); // Modificado aqui
+            }
+            keyboardRow.appendChild(button);
+        });
+        keyboard.appendChild(keyboardRow);
+    });
 
-  numericKeys.forEach(key => {
-      const button = document.createElement('button');
-      button.textContent = key;
-      numericKeyboard.appendChild(button);
-  });
+    numericKeys.forEach(key => {
+        const button = document.createElement('button');
+        button.textContent = key;
+        numericKeyboard.appendChild(button);
+    });
 
-  // Adicionando o mini-computador ao painel de controle de voo
-  const flightControlPanel = document.getElementById('flight-control-panel');
-  if (flightControlPanel) {
-      flightControlPanel.appendChild(miniComputer);
-  }
+    // Adicionando o mini-computador ao painel de controle de voo
+    const flightControlPanel = document.getElementById('flight-control-panel');
+    if (flightControlPanel) {
+        flightControlPanel.appendChild(miniComputer);
+    }
 
   // Limitar número de caracteres e linhas
   let lines = [''];
   const maxCharsPerLine = 20;
-  const maxLines = 4;
+  const maxLines = 10000;
 
   // Função para capturar cliques nos botões e exibir no mini-computador
   const buttons = miniComputer.querySelectorAll('button');
@@ -98,38 +98,159 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 //interruptores
-    // Adicionando o painel de interruptores
-    const switchPanel = document.createElement('div');
-    switchPanel.classList.add('switch-panel');
-    const switches = [
-        'Ignição',
-        'Power',
-        'Motor 1',
-        'Motor 2',
-        'Motores Aux',
-        'Elétrica',
-        'Hidráulica',
-        'Modo Eco',
-        'Modo Turbo',
-        'Modo Decolagem',
-        'Modo Cruzeiro'
-    ];
+    // Criação do painel de interruptores
+const switchPanel = document.createElement('div');
+switchPanel.classList.add('switch-panel');
+// Criação dos interruptores
+const switches = [
+    'Ignição',
+    'Power',
+    'Motor 1',
+    'Motor 2',
+    'Motores Aux',
+    'Elétrica',
+    'Hidráulica',
+    'Modo Eco',
+    'Modo Turbo',
+    'Modo Decolagem',
+    'Modo Cruzeiro'
+];
 
-    switches.forEach(switchLabel => {
-        const switchContainer = document.createElement('div');
-        switchContainer.classList.add('switch-container');
-        const input = document.createElement('input');
-        input.type = 'checkbox';
-        const switchDiv = document.createElement('div');
-        switchDiv.classList.add('switch');
-        const label = document.createElement('div');
-        label.classList.add('switch-label');
-        label.textContent = switchLabel;
-        switchDiv.appendChild(input);
-        switchContainer.appendChild(switchDiv);
-        switchContainer.appendChild(label);
-        switchPanel.appendChild(switchContainer);
+switches.forEach(switchLabel => {
+    const switchContainer = document.createElement('div');
+    switchContainer.classList.add('switch-container');
+
+    const button = document.createElement('button');  // Usando um botão em vez de uma caixa de seleção
+    button.textContent = switchLabel;
+    button.classList.add('switch');
+
+    switchContainer.appendChild(button);
+    switchPanel.appendChild(switchContainer);
+});
+
+miniComputer.appendChild(switchPanel);
+
+// Acionar o interruptor de ignição
+const switchButtons = switchPanel.querySelectorAll('button');
+const ignitionSwitch = switchButtons[0]; // Pegando o primeiro botão, que deve ser o de Ignição
+const electricSwitch = switchButtons[5]; //eletrica
+const hidraulicoSwitch = switchButtons[6]; //hidráulico
+const motor1Switch = switchButtons[2]; // motor 1
+const motor2Switch = switchButtons[3]; // motor 2
+console.log('Interruptor de ignição:', ignitionSwitch); // Novo log
+
+electricSwitch.disabled = true; // O botão Elétrica começa desativado
+hidraulicoSwitch.disabled = true;
+motor1Switch.disabled = true;
+motor2Switch.disabled = true
+
+if (ignitionSwitch) {
+    ignitionSwitch.addEventListener('click', function() {
+        console.log('Interruptor de ignição acionado');
+        this.classList.add('active');  // Adiciona a classe 'active' ao botão
+        this.disabled = true;  // Desativa o botão
+
+        // Iniciar animação de carregamento na tela do computador
+        let percentage = 0;
+        const loadingInterval = setInterval(() => {
+            // Atualizar texto na tela do computador
+            computerOutput.textContent = `Carregando sistemas... ${percentage}%`;
+            console.log(`Carregando sistemas... ${percentage}%`);
+            percentage++;
+            if (percentage > 100) {
+                clearInterval(loadingInterval);
+                console.log('Carregamento completo');
+                // Adicionar a mensagem na tela do computador após o carregamento ser concluído
+                lines.push('Sistemas de ignição carregados, ligue o sistema elétrico');
+                lines.push('');  // Adicionar uma nova linha vazia
+                computerOutput.textContent = lines.join('\n');
+                electricSwitch.disabled = false; // Ativa o botão Elétrica após o carregamento dos sistemas de ignição
+            }
+        }, 30);
     });
+}
 
-    miniComputer.appendChild(switchPanel);
+// Acionar o interruptor Elétrica
+if (electricSwitch) {
+    electricSwitch.addEventListener('click', function() {
+        console.log('Interruptor Elétrica acionado');
+        this.classList.add('active');  // Adicionar a classe 'active' ao botão
+        this.disabled = true;  // Desativar o botão
+
+        // Iniciar animação de carregamento na tela do computador
+        let percentage = 0;
+        const loadingInterval = setInterval(() => {
+            // Atualizar texto na tela do computador
+            computerOutput.textContent = `Carregando sistemas elétricos... ${percentage}%`;
+            console.log(`Carregando sistemas elétricos... ${percentage}%`);
+            percentage++;
+            if (percentage > 100) {
+                clearInterval(loadingInterval);
+                console.log('Carregamento completo');
+                // Adicionar a mensagem na tela do computador após o carregamento ser concluído
+                lines.push('Sistemas elétricos carregados, ligue o sistema hidráulico');
+                lines.push('');  // Adicionar uma nova linha vazia
+                computerOutput.textContent = lines.join('\n');
+                computerOutput.scrollTop = computerOutput.scrollHeight;
+                hidraulicoSwitch.disabled = false; // Ativa o botão Hidráulico após o carregamento dos sistemas elétricos
+                motor1Switch.disabled = false; // Ativa o botão Motor 1 após o carregamento dos sistemas elétricos
+                motor2Switch.disabled = false; // Ativa o botão Motor 2 após o carregamento dos sistemas elétricos
+            }
+        }, 20);
+    });
+}
+
+// Acionar o interruptor Hidraulico
+if (hidraulicoSwitch) {
+    hidraulicoSwitch.addEventListener('click', function() {
+        this.classList.add('active');  // Adicionar a classe 'active' ao botão
+        this.disabled = true;  // Desativar o botão
+
+        // Iniciar animação de carregamento na tela do computador
+        let percentage = 0;
+        const loadingInterval = setInterval(() => {
+            // Atualizar texto na tela do computador
+            computerOutput.textContent = `Carregando sistemas hidraulicos... ${percentage}%`;
+            console.log(`Carregando sistemas hidraulicos... ${percentage}%`);
+            percentage++;
+            if (percentage > 100) {
+                clearInterval(loadingInterval);
+                console.log('Carregamento completo');
+                // Adicionar a mensagem na tela do computador após o carregamento ser concluído
+                lines.push('Sistemas hidraulicos carregados, ligue os motores');
+                lines.push('');  // Adicionar uma nova linha vazia
+                computerOutput.textContent = lines.join('\n');
+                computerOutput.scrollTop = computerOutput.scrollHeight;
+            }
+        }, 40);
+    });
+}
+
+// Acionar o interruptor do Motor 1
+if (motor1Switch) {
+    motor1Switch.addEventListener('click', function() {
+        console.log('Interruptor Motor 1 acionado');
+        this.classList.add('active');  // Adicionar a classe 'active' ao botão
+        this.disabled = true;  // Desativar o botão
+        lines.push('Motor 1 acionado');
+        lines.push('');  // Adicionar uma nova linha vazia
+        computerOutput.textContent = lines.join('\n');
+        computerOutput.scrollTop = computerOutput.scrollHeight;
+    });
+}
+
+if (motor2Switch) {
+    motor2Switch.addEventListener('click', function() {
+        console.log('Interruptor Motor 2 acionado');
+        this.classList.add('active');  // Adicionar a classe 'active' ao botão
+        this.disabled = true;  // Desativar o botão
+        lines.push('Motor 2 acionado');
+        lines.push('');  // Adicionar uma nova linha vazia
+        computerOutput.textContent = lines.join('\n');
+        computerOutput.scrollTop = computerOutput.scrollHeight;
+    });
+}
+
+
+
 });
